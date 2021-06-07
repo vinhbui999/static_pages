1cf828703da346571a82c7ce8322295128fa3799
vinhbui999 <vinhbui999@gmail.com>
2021-06-07 13:40:35 +0700

    Add a full title helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,14 @@
 module StaticPagesHelper
+
+    def full_title(page_title = '') #if some page do not provide the title, it will be default
+        #this func only support for static controller only
+        base_title = "Ruby on Rails Tutorial Sample App"
+
+        if page_title.empty?
+            base_title
+        else
+            page_title += "|" + base_title
+        end
+    end
+
 end
