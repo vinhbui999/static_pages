ac0bf9058aa10c018891e653711a85042d0b69f0
vinhbui999 <vinhbui999@gmail.com>
2021-06-10 12:46:17 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def new #automatically call new.html
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create #handle signup failure
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" #display temporary message
+      redirect_to @user #redirect to user_url(@user)
+    else
+      render 'new' #rerender if invalid infomation
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def gravatar_for(user, options = {size: 80})
+        size = options[:size]
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        puts gravatar_url
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
